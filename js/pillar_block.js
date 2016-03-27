b864ec1bbcf7b6d2fecb058a1a6350991924188a
David Basalla <davidbasalla@gmail.com>
2016-03-27 02:12:56 +0100

    Update Pillars to be Cylinders

--- js/pillar_block.js
+++ js/pillar_block.js
@@ -9,7 +9,7 @@ var PillarBlock = function (x, z, rotation, scene) {
   this.material = new BABYLON.StandardMaterial("pillar", this.scene);
   this.material.diffuseColor = new BABYLON.Color3(.7, .7, .7);
 
-  this.shape = BABYLON.Mesh.CreateBox('scene_elem', 0.5, this.scene);
+  this.shape = BABYLON.Mesh.CreateCylinder('scene_elem', 0.5, .5, .5, 6, this.scene);
 
   SceneElement.call(this);
 };
